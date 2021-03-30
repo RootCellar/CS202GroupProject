3fc62c252a1e2a6ab3ee192df55418c64b9b4cb9
RootCellar <RootCellar9877@GMail.com>
2021-03-30 10:35:08 -0800

    olcPixelGameEngine sample

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,102 @@
-/*
- * main.cpp
- * 3/25/2021
- * 
- * This project is written by:
- * Darian Marvel
- * Uddeep Karki
- * Aleks McCormick
- * Nathon Damon
- *
-*/
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
 
-//Our Headers
-#include "debug.h"
+class Example : public olc::PixelGameEngine
+{
+public:
+	Example()
+	{
+		sAppName = "Example";
+	}
 
-//C++ Headers
-#include <iostream>
+	float fTargetFrameTime = 1.0f / 50.0f; // Virtual FPS of 50fps
+  float fAccumulatedTime = 0.0f;
 
-//Using statements
-using std::cout;
-using std::endl;
+	int x = 0;
+	int y = 0;
 
-int main() {
 
-    debug("debug mode is enabled!");
+public:
+	bool OnUserCreate() override
+	{
+		// Called once at the start, so create things here
 
+		x = ScreenWidth() / 2;
+		y = ScreenHeight() / 2;
+
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+
+		fAccumulatedTime += fElapsedTime;
+		if (fAccumulatedTime >= fTargetFrameTime)
+		{
+			fAccumulatedTime -= fTargetFrameTime;
+			//fElapsedTime = fTargetFrameTime;
+		}
+		else
+			return true; // Don't do anything this frame
+
+		// called once per frame
+		/*
+		for (int x = 0; x < ScreenWidth(); x++)
+			for (int y = 0; y < ScreenHeight(); y++)
+				Draw(x, y, olc::Pixel(rand() % 255, rand() % 255, rand()% 255));
+		*/
+
+		for (int x = 0; x < ScreenWidth(); x++)
+			for (int y = 0; y < ScreenHeight(); y++)
+				Draw(x, y, olc::Pixel(0, 0, 0));
+
+		for (int x = 0; x < 1000; x+=50)
+			for (int y = 0; y < 1000; y+=50)
+				drawPixel(x, y, 255, 255, 255);
+
+		//x++;
+		drawPixel(x, y, 255, 0, 0 );
+		drawPixel( 75, 75, 0, 255, 0 );
+
+		//if( x > ScreenWidth() ) x=0;
+
+		if(GetKey(olc::A).bHeld) {
+			x--;
+		}
+		if(GetKey(olc::D).bHeld) {
+			x++;
+		}
+
+		if(GetKey(olc::W).bHeld) {
+			y--;
+		}
+		if(GetKey(olc::S).bHeld) {
+			y++;
+		}
+
+		return true;
+	}
+
+	void drawPixel(int xPos, int yPos, int r, int g, int b) {
+		if(xPos < 0 || yPos < 0) return;
+
+		xPos -= x;
+		xPos += (ScreenWidth() / 2);
+
+		yPos -= y;
+		yPos += (ScreenHeight() / 2);
+
+		Draw(xPos, yPos, olc::Pixel( r, g, b) );
+	}
+
+};
+
+
+int main()
+{
+	Example demo;
+	if (demo.Construct(256, 240, 4, 4))
+		demo.Start();
+
+	return 0;
 }
