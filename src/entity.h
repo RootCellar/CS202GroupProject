781942bb1b369c214fc9991c979c8a01a3f02720
Nathan Damon <nidamon@alaska.edu>
2021-04-01 12:14:52 -0800

    Added a move function that takes a speed and an angle and returns an x and y speed in the given direction in entity.h.

--- src/entity.h
+++ src/entity.h
@@ -1,6 +1,13 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
+#define PI 3.14159265
+struct vec2D
+{
+    double _x;
+    double _y;
+};
+
 class Entity {
 private:
   int xPos;
@@ -12,5 +19,15 @@ public:
 
   virtual void update() = 0;
 };
+
+
+
+vec2D move(const double speed, const double angle)
+{
+    vec2D v;
+    v._x = (cos(angle) * speed);
+    v._y = (sin(angle) * speed);
+    return v;
+}
 
 #endif
