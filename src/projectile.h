7066e20af9b97505b822f68db6de7b66ecd82143
uddeepk <uddeepk@gmail.com>
2021-04-02 03:22:10 -0800

    changed speed for testing

--- src/projectile.h
+++ src/projectile.h
@@ -11,8 +11,8 @@ private:
     // Since, comparison of doulbes difficult, will be using the max distance to travel for life
     // time of projectile
 
-    double _speed = 0; // 0 for testing
-    double _radius;
+    double _speed = .0009; // 0 for testing
+    double _radius = 5;
 public:
     Projectile() ; // Random ??
     Projectile(olc::vd2d sPos, olc::vd2d ePos): _position(sPos), _endPosition(ePos)
@@ -24,10 +24,10 @@ public:
 
     void drawSelf(olc::PixelGameEngine* pge)
     {
-        pge->FillCircle(_position, 3);
+        pge->FillCircle(_position, _radius, olc::MAGENTA);
     }
   //update the projectile, move it etc.
-  void update(olc::PixelGameEngine* pge)
+  virtual void update(olc::PixelGameEngine* pge) override
   {
         // Updating position of projectile
         _position += _direction * _speed;
