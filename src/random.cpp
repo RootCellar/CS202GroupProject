e959413b8a823e54fd603a8bb79a037ec6d224cb
RootCellar <RootCellar9877@GMail.com>
2021-04-13 20:58:17 -0800

    RNG work

--- src/random.cpp
+++ src/random.cpp
@@ -1,1 +1,9 @@
 #include "random.h"
+#include <random>
+
+mt19937 Random::gen(Random::dev());
+
+int randInt(int min, int max) {
+  std::uniform_int_distribution<int> dist(min,max);
+  return dist(Random::getGen());
+}
--- src/random.h
+++ src/random.h
@@ -4,12 +4,18 @@
 #include <random>
 
 using std::mt19937;
+using std::random_device;
 
 class Random {
 
 private:
 
+  static random_device dev;
   static mt19937 gen;
+
+public:
+
+  static auto getGen() { return gen; }
 
 };
 
